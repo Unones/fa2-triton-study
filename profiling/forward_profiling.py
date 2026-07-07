be05189c558b686dc8621306451125a3c6b6372a
Unones <229073236+Unones@users.noreply.github.com>
2026-07-07 22:51:45 +0200

    Change to profiling function

--- profiling/forward_profiling.py
+++ profiling/forward_profiling.py
@@ -6,31 +6,29 @@ from torch.nn.attention import SDPBackend, _sdpa_kernel_variadic
 
 
 def main():
+    H = 32
+    B = 32
     N = 4096
     d = 64
 
     dtype = torch.bfloat16
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    q_tensor = torch.randn((N, d), dtype=dtype, device=device)
-    k_tensor = torch.randn((N, d), dtype=dtype, device=device)
-    v_tensor = torch.randn((N, d), dtype=dtype, device=device)
-
-    q_tensor_torch = q_tensor.reshape(1, 1, N, d)
-    k_tensor_torch = k_tensor.reshape(1, 1, N, d)
-    v_tensor_torch = v_tensor.reshape(1, 1, N, d)
+    q_tensor = torch.randn((H, B, N, d), dtype=dtype, device=device)
+    k_tensor = torch.randn((H, B, N, d), dtype=dtype, device=device)
+    v_tensor = torch.randn((H, B, N, d), dtype=dtype, device=device)
 
     # Warmup (hors zone profilée)
     fa2_forward(q_tensor, k_tensor, v_tensor)
     with _sdpa_kernel_variadic(SDPBackend.FLASH_ATTENTION):
-        F.scaled_dot_product_attention(q_tensor_torch, k_tensor_torch, v_tensor_torch)
+        F.scaled_dot_product_attention(q_tensor, k_tensor, v_tensor)
     torch.cuda.synchronize()
 
     # Zone profilée
     torch.cuda.cudart().cudaProfilerStart() #type:ignore
     fa2_forward(q_tensor, k_tensor, v_tensor)
     with _sdpa_kernel_variadic(SDPBackend.FLASH_ATTENTION):
-        F.scaled_dot_product_attention(q_tensor_torch, k_tensor_torch, v_tensor_torch)
+        F.scaled_dot_product_attention(q_tensor, k_tensor, v_tensor)
     torch.cuda.synchronize()
     torch.cuda.cudart().cudaProfilerStop()  #type:ignore
 
